f8002b9a0de0cc5095be2e6b92ad4f683044604b
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-09 16:32:56 +0800

    Update banner.js

--- resource/js/banner.js
+++ resource/js/banner.js
@@ -1,7 +1,7 @@
 // JScript source code
 var bannertext = '<div style="position: relative; top: 0px; left: 0px; padding-left: 0; background-color: #000000">'
 bannertext += '\n<div style="position:relative;top:10px;left:0px;padding-left:20;background-color:#000000" >'
-bannertext += '\n<img src="images/banner.jpg">'
+bannertext += '\n<img src="/resource/images/banner.jpg">'
 bannertext += '\n</div> '
 bannertext += '\n<div style="position:absolute;top:0px;left:0px;bottom:10px;padding-left:0;background-color:#000000">'
 bannertext += '\n<h2 style="color:white;"></h3>'
