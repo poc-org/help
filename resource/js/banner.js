c57c5c2f8acd27a92fe08ae6fb02cb7c20588343
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-09 15:50:28 +0800

    banner JS File

---
+++ resource/js/banner.js
@@ -0,0 +1,11 @@
+// JScript source code
+var bannertext = '<div style="position: relative; top: 0px; left: 0px; padding-left: 0; background-color: #000000">'
+bannertext += '\n<div style="position:relative;top:10px;left:0px;padding-left:20;background-color:#000000" >'
+bannertext += '\n<img src="../../image/banner.jpg">'
+bannertext += '\n</div> '
+bannertext += '\n<div style="position:absolute;top:0px;left:0px;bottom:10px;padding-left:0;background-color:#000000">'
+bannertext += '\n<h2 style="color:white;"></h3>'
+bannertext += '\n</div>'
+bannertext += '\n</div>'
+document.write(bannertext);
+
